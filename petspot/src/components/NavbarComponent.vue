3733843d47db18cc817c036085137df45e4536e7
franceschini <andrefranceschini@outlook.com.br>
2024-10-09 20:13:56 -0300

    refactor: coloquei o navhelp em escopo, em um component shared a parte. e melhorei o visual do nav help e sua responsividade.

--- petspot/src/components/NavbarComponent.vue
+++ petspot/src/components/NavbarComponent.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import NavHelp from "./shared/NavHelp.vue";
 import NavBrand from "./shared/NavBrand.vue";
 import NavItem from "../components/shared/NavItem.vue";
 import NavSearchForm from "../components/shared/NavSearchForm.vue";
@@ -40,39 +41,10 @@ const closeOffcanvas = () => {
     <NavBrand />
     <!-- NavItem -->
     <NavItem />
-
     <!-- Seção de busca -->
     <NavSearchForm />
     <!-- Seção de ajuda e perfil -->
-    <ul
-      class="navbar-nav d-flex flex-row align-items-center justify-content-center gap-1 gap-md-2 gap-lg-4"
-    >
-      <li class="nav-item p-2">
-        <a
-          class="d-flex align-items-center text-decoration-none gap-1"
-          data-bs-toggle="offcanvas"
-          href="#offCanvasAjuda"
-          role="button"
-          aria-controls="offCanvasAjuda"
-        >
-          <ion-icon name="help-buoy" class="nav-icon"></ion-icon>
-          <span class="nav-icon-span text-nowrap d-none d-md-block">Ajuda</span>
-        </a>
-      </li>
-      <li class="nav-item p-2">
-        <a
-          class="d-flex align-items-center text-decoration-none gap-1"
-          data-bs-toggle="offcanvas"
-          href="#offCanvasRegistrar"
-          role="button"
-        >
-          <ion-icon name="person-circle" class="nav-icon"></ion-icon>
-          <span class="nav-icon-span text-nowrap d-none d-md-block"
-            >Registrar-se</span
-          >
-        </a>
-      </li>
-    </ul>
+    <NavHelp />
   </nav>
 
   <!-- Offcanvas Ajuda -->
@@ -167,17 +139,6 @@ const closeOffcanvas = () => {
 
 <style scoped>
 /* Navbar Components */
-
-.nav-icon {
-  font-size: 2.5rem;
-  color: var(--brand-color);
-}
-
-.nav-icon-span {
-  font-size: 1.5rem;
-  text-decoration: none;
-  color: var(--brand-color);
-}
 
 @media (max-width: 767.98px) {
   .navbar {
---
+++ petspot/src/components/shared/NavHelp.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts"></script>
+<template>
+  <ul
+    class="navbar-nav d-flex flex-row align-items-center justify-content-center gap-1 gap-md-2 gap-lg-4"
+  >
+    <li class="nav-item p-2">
+      <a
+        class="d-flex align-items-center text-decoration-none gap-1"
+        data-bs-toggle="offcanvas"
+        href="#offCanvasAjuda"
+        role="button"
+        aria-controls="offCanvasAjuda"
+      >
+        <ion-icon name="help-buoy" class="nav-icon"></ion-icon>
+        <span class="nav-icon-span text-nowrap d-none d-md-block">Ajuda</span>
+      </a>
+    </li>
+    <li class="nav-item p-2">
+      <a
+        class="d-flex align-items-center text-decoration-none gap-1"
+        data-bs-toggle="offcanvas"
+        href="#offCanvasRegistrar"
+        role="button"
+      >
+        <ion-icon name="person-circle" class="nav-icon"></ion-icon>
+        <span class="nav-icon-span text-nowrap d-none d-md-block"
+          >Registrar-se</span
+        >
+      </a>
+    </li>
+  </ul>
+</template>
+<style scoped>
+.nav-icon {
+  font-size: 2.5rem;
+  color: var(--brand-color);
+}
+
+.nav-icon-span {
+  font-size: 1.5rem;
+  text-decoration: none;
+  color: var(--brand-color);
+}
+</style>
