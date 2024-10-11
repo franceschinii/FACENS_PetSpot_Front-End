cb4aa9119fe1084ffdf0497fa794d25fc9e89b9c
franceschini <andrefranceschini@outlook.com.br>
2024-10-11 17:16:42 -0300

    refactor/build: removi o uso do vuetify, pois estava conflitando com o bootstrap.

--- petspot/src/pages/LandingPage.vue
+++ petspot/src/pages/LandingPage.vue
@@ -4,16 +4,28 @@ import HeaderComponent from "../layouts/HeaderComponent.vue";
 
 <template>
   <HeaderComponent />
-  <section class="p-5">
+  <section id="presentation-card" class="p-5">
     <div class="container">
-      <div class="row d-flex justify-content-between align-items-center">
-        <div class="col-6 col-md-6 bg-light p-5 rounded-5">
+      <div
+        class="row d-flex flex-column-reverse flex-md-row justify-content-between align-items-center"
+      >
+        <div
+          class="col-12 col-md-6 bg-light p-md-5 p-4 rounded-5 w-100 w-md-50"
+        >
           <h1>Um lugar para cuidar do seu Pet!</h1>
           <p>A plataforma inteligente, criada com amor para o seu pet.</p>
-          <a href="#" class="btn btn-primary">Conheça mais</a>
+          <a href="#" class="btn btn-primary"
+            ><span class="d-flex align-center text-light gap-2"
+              >Conheça mais<ion-icon
+                name="paw-outline"
+                class="button-icon"
+              ></ion-icon></span
+          ></a>
         </div>
-        <div class="col-6 col-md-6 d-flex justify-center align-items-center">
-          <div class="card bg-info p-4 rounded rounded-circle w-75 h-75">
+        <div class="col-12 col-md-6 d-flex justify-center align-items-center">
+          <div
+            class="card bg-info p-4 rounded-circle w-md-75 h-md-75 w-75 h-75"
+          >
             <img
               src="../assets/images/Cachorro-PNG-1.png"
               alt="Cachorro feliz"
@@ -26,4 +38,8 @@ import HeaderComponent from "../layouts/HeaderComponent.vue";
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+.button-icon {
+  font-size: 1.5rem;
+}
+</style>
--- petspot/src/scripts/ts/main.ts
+++ petspot/src/scripts/ts/main.ts
@@ -3,17 +3,11 @@ import "../../styles/css/custom.css"; // Bootstrap customizado
 import "../../styles/css/style.css"; // Arquivo de estilo personalizado
 import App from "../../App.vue";
 
-// Importando Vuetify
-import vuetify from "../../plugins/vuetify.ts";
-
 // Importando o Router
 import router from "../../router/index.ts";
 
 // Criando e montando o aplicativo Vue
 const app = createApp(App);
-
-// Usando Vuetify no aplicativo
-app.use(vuetify);
 
 // Usando o Vue Router no aplicativo
 app.use(router);
