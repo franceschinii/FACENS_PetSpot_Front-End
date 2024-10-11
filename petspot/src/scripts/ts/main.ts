ccfc16f08d68e83248613365640603cacba62daa
franceschini <andrefranceschini@outlook.com.br>
2024-10-11 17:17:18 -0300

    fix: corrigi a main.ts para evitar conflitos.

--- petspot/src/scripts/ts/main.ts
+++ petspot/src/scripts/ts/main.ts
@@ -1,13 +1,19 @@
-import { createApp } from "vue";
+ import { createApp } from "vue";
 import "../../styles/css/custom.css"; // Bootstrap customizado
 import "../../styles/css/style.css"; // Arquivo de estilo personalizado
 import App from "../../App.vue";
+
+// Importando Vuetify
+import vuetify from "../../plugins/vuetify.ts";
 
 // Importando o Router
 import router from "../../router/index.ts";
 
 // Criando e montando o aplicativo Vue
 const app = createApp(App);
+
+// Usando Vuetify no aplicativo
+app.use(vuetify);
 
 // Usando o Vue Router no aplicativo
 app.use(router);
