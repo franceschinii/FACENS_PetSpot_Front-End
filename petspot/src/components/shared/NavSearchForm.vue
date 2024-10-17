1705ed0bc0594c34a1073ca8848335de4bef561a
franceschini <andrefranceschini@outlook.com.br>
2024-10-16 21:03:34 -0300

    feat: modal de pesquisa, que utiliza de um modal e o componente de pesquisa da tela xl

--- petspot/src/components/shared/NavSearchForm.vue
+++ petspot/src/components/shared/NavSearchForm.vue
@@ -24,6 +24,8 @@
   >
     <button
       class="btn btn-primary border-2 rounded rounded-pill d-flex align-items-center justify-content-center text-light gap-2 p-2"
+      data-bs-toggle="modal"
+      data-bs-target="#searchModal"
     >
       Buscar
       <ion-icon name="search-outline" class="text-light"></ion-icon>
@@ -34,10 +36,62 @@
   >
     <button
       class="btn btn-primary border-2 rounded rounded-pill d-flex align-items-center justify-content-center text-light gap-2 p-2"
+      data-bs-toggle="modal"
+      data-bs-target="#searchModal"
     >
       <ion-icon name="search-outline" class="text-light"></ion-icon>
     </button>
   </ul>
+  <!-- Modal de Pesquisa -->
+  <div
+    class="modal fade"
+    id="searchModal"
+    tabindex="-1"
+    aria-labelledby="searchModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="searchModalLabel">Pesquisar</h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div
+          class="modal-body d-flex align-items-center justify-content-center"
+        >
+          <form class="d-flex gap-2" role="search">
+            <input
+              class="form-control"
+              type="search"
+              placeholder="Pesquisar..."
+              aria-label="Pesquisar"
+            />
+            <button
+              class="btn btn-outline-primary d-flex align-items-center gap-2"
+              type="submit"
+            >
+              <ion-icon name="search" class="text-primary"></ion-icon>
+            </button>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            Cancelar
+          </button>
+          <button type="submit" class="btn btn-primary">Pesquisar</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped>
 ion-icon {
